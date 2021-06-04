b455043708a7c18239e53a6420e8b4a56e632c04
Sungho Bang <crosstreet74@gmail.com>
2021-06-04 13:03:17 +0900

    write catalog items reading from json in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,20 @@
-const car = { type: "Fiat", model: "500", color: "white" };
 let flag = true;
 let isDark = false;
-let catalog = [{ type: 'Sports Sedan', model: 'Stinger', brand: 'Kia' },
-{ type: 'Sports Utility', model: 'Cayenne', brand: 'Porsche' },
-{ type: 'Coupe', model: '4-door Coupe', brand: 'Mercedes-Benz' }
-];
+let catalog = {
+    'vehicles': [{ type: 'Sports Sedan', model: 'Stinger', brand: 'Kia', img_url: 'assets/img/catalog-kia_stinger.jpg' },
+        { type: 'Sports Utility', model: 'Cayenne', brand: 'Porsche', img_url: 'assets/img/catalog-porsche_cayenne.jpg' },
+        { type: 'Coupe', model: '4-door Coupe', brand: 'Mercedes-Benz', img_url: 'assets/img/catalog-mercedes_benz_4door_coupe.jpg' }
+    ]
+}
 
 var x;
-for (x = 0; x < catalog.length; x++) {
-    doc = '<li>' + catalog[x].brand + ' ' + catalog[x].type + ' ' + catalog[x].brand + '</li>';
-    document.getElementById('catalog_detail').innerHTML += doc;
+for (x in catalog.vehicles) {
+    doc = '<div class="catalog_item">' +
+        '<img class="catalog_img" src="' + catalog.vehicles[x].img_url + '"> <br/>' +
+        catalog.vehicles[x].brand + '<br/>' + catalog.vehicles[x].model + '<br/>' + catalog.vehicles[x].type +
+        '</div>';
+
+    document.getElementById('catalog_list').innerHTML += doc;
 }
 
 function setCar(type, model, color) {
@@ -20,25 +25,24 @@ function setCar(type, model, color) {
     document.getElementById("car_icon").style.transition = '0.5s';
     document.getElementById("car_preview").style.display = 'initial'
 }
+
 function myFunc() {
     if (flag == true) {
         setCar("Volvo", "EX60", "gray");
         flag = false;
-    }
-    else if (flag == false) {
+    } else if (flag == false) {
         setCar("Fiat", "500", "black");
         flag = true;
-    }
-    else {
+    } else {
 
     }
 }
+
 function toggleDarkTheme() {
     if (isDark === false) {
         setDarkTheme()
         isDark = true;
-    }
-    else if (isDark === true) {
+    } else if (isDark === true) {
         setLightTheme()
         isDark = false;
     }
@@ -54,6 +58,12 @@ function setLightTheme() {
     document.getElementById('lightThemeIcon').style.display = 'none'
     document.getElementById('spanToggleTheme').style.color = '#eee'
     document.getElementById('btnToggleTheme').style.backgroundColor = '#222'
+
+    let len = document.getElementsByClassName('catalog_item').length
+    for (let i = 0; i < len; i++) {
+        document.getElementsByClassName('catalog_item')[i].style.backgroundColor = 'dimgray'
+        document.getElementsByClassName('catalog_item')[i].style.color = '#eee'
+    }
 }
 
 function setDarkTheme() {
@@ -66,4 +76,10 @@ function setDarkTheme() {
     document.getElementById('lightThemeIcon').style.display = 'initial'
     document.getElementById('spanToggleTheme').style.color = '#222'
     document.getElementById('btnToggleTheme').style.backgroundColor = '#eee'
+
+    let len = document.getElementsByClassName('catalog_item').length
+    for (let i = 0; i < len; i++) {
+        document.getElementsByClassName('catalog_item')[i].style.backgroundColor = '#eee'
+        document.getElementsByClassName('catalog_item')[i].style.color = '#222'
+    }
 }
