2e5c815a782c6e023ede5658501226281b59b789
Sungho Bang <crosstreet74@gmail.com>
2021-06-08 17:52:30 +0900

    1st attempt for iterative Item component

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,14 +1,32 @@
 import Item from "./Item.js"
-import { Fragment } from "react"
+import { Fragment, useState } from "react"
 
 export default function Catalog() {
+    let [brand, setBrand] = useState([
+        "Kia",
+        "Porsche",
+        "Mercedes-Benz",
+        "Jeep",
+        "Land Rover"
+    ]);
+
+    let [model, setModel] = useState([
+        "Stinger",
+        "Cayenne",
+        "GT 4-Door Coupe",
+        "Renegade Longitude",
+        "Range Rover"
+    ]);
+
     return (
         <Fragment>
-            <Item brand="Land Rover" model="Range Rover"></Item>
-            <Item brand="Kia" model="Stinger"></Item>
-            <Item brand="Porsche" model="Cayenne" ></Item>
-            <Item brand="Mercedes-Benz" model="GT 4-Door Coupe" ></Item>
-            <Item brand="Jeep" model="Renegade Longitude"></Item>
+            {
+                brand.map((item, index) => {
+                    return (
+                        <Item brand={item} key={index}></Item>
+                    )
+                })
+            }
         </Fragment>
     )
 }
